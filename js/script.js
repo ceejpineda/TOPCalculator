af9a75b594b479067e02d486ae8783ec7a8f0531
CJ <pinedacj1020@gmail.com>
2022-08-21 19:59:55 +0800

    added styles and some functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 let btn = document.querySelectorAll('.buttons');
+let screen = document.getElementById('screenText')
 
 btn.forEach(btns => {
-    btns.addEventListener('mousemove', e => {
-      let rect = e.target.getBoundingClientRect();
-      let x = e.clientX - rect.left;
-      let y = e.clientY - rect.top;
-      btns.style.setProperty('--x', x + 'px');
-      btns.style.setProperty('--y', y + 'px');
+    btns.addEventListener('click', e => {
+        screen.innerText += btns.innerText
     });
 });
