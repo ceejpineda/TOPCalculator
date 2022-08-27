c0c5a576cefe15344d3f22a191d20920bb6b2aa1
CJ <pinedacj1020@gmail.com>
2022-08-28 03:14:01 +0800

    added some logic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-let btn = document.querySelectorAll('.buttons');
-let screen = document.getElementById('screenText')
+let btn = document.querySelectorAll('div.buttons');
+let btnContent = document.querySelectorAll('div.buttons > span');
+let screen = document.getElementById('screenText');
+const digits = [0,1,2,3,4,5,6,7,8,9];
 
 btn.forEach(btns => {
     btns.addEventListener('click', e => {
-        screen.innerText += btns.innerText
+        if(btns.innerText in digits){
+            screen.innerText += btns.innerText;
+        }
+        if(btns.innerText == "+"){
+            console.log("hello");
+        }
+        if(btns.innerText == "-"){
+            console.log("hello");
+        }
+        if(btns.innerText == "÷"){
+            console.log("hello");
+        }
+        if(btns.innerText == "×"){
+            console.log("hello");
+        }
+
     });
 });
